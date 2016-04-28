3e00ba9873d8d8a80b0c960d9c591c92fadabd4c
Marius Baci <marius.baci@yahoo.com>
2016-04-28 17:30:02 +0300

    Added all the changes

--- client/client.js
+++ client/client.js
@@ -1,1 +1,11 @@
+import React from 'react'
+//the same as var React = require('react') in ES5
+import { render } from 'react-dom'
+//the same as var render = require('react-dom').render in ES5
+import App from '../components/App'
+
+render(
+	<App/>,
+	document.getElementById('app')
+)
 
